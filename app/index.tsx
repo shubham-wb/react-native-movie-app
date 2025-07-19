d75fe997a435c8aa24fecec7862921e1f2b6dde5
tensaidevx <shubham.gupta.npd@gmail.com>
2025-07-19 17:36:50 +0530

    'fix/tailwindcss'

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,10 @@
 import { Text, View } from "react-native";
-import "./globals.css";
 
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
+      <Text className="text-5xl  text-dark-200 font-bold">
+        Welcome
       </Text>
     </View>
   );
